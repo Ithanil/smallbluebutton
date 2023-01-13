bf2de98b87e33aca6dc612c29663e0b15cb80695
Jan Kessler <jakessle@t-online.de>
2023-01-13 16:34:35 +0100

    working toggle + added extra arguments/flags

--- smallbluebutton.py
+++ smallbluebutton.py
@@ -16,17 +16,26 @@ def init_argparse() -> argparse.ArgumentParser:
         usage="%(prog)s [OPTIONS] [WindowTitle]...",
         description="Toggle&check mute status in BigBlueButton windows",
     )
-    parser.add_argument("-a", "--alpha", type=float, default='0.85')
+    parser.add_argument("-a", "--alpha", type=float, default="0.85")
+    parser.add_argument("-s", "--sleep", type=float, default="0.1")
+    parser.add_argument('-r', '--raise', dest="winRaise", action='store_true')
     parser.add_argument("title")
     return parser
 
-def toggleMute(winTitle):
-    subprocess.run(["xdotool", "search", str(winTitle), "windowfocus"])
-    #subprocess.run(["xdotool", "search", str(winTitle), "windowfocus", "sleep", "0.1", "key", "alt+M"],
- #                   shell=True, check=True)
+def toggleMute(winTitle, sleepTime) -> None:
+    subprocess.run(["xdotool", "search", str(winTitle), "windowfocus",
+                    "sleep", str(sleepTime), "key", "alt+M"],
+                    check=True)
 
-def sbbCallBack(winTitle):
-    toggleMute(winTitle)
+def raiseWindow(winTitle) -> None:
+    subprocess.run(["xdotool", "search", str(winTitle), "windowraise"],
+                    check=True)
+
+
+def sbbCallBack(winTitle, sleepTime, winRaise) -> None:
+    toggleMute(winTitle, sleepTime)
+    if winRaise:
+        raiseWindow(winTitle)
 
 def main() -> None:
     parser = init_argparse()
@@ -39,7 +48,7 @@ def main() -> None:
 
     # Create button and image
     sbb = tk.Button(win.root, image = bbbLogo, borderwidth = 0,
-                        command = lambda: sbbCallBack(args.title))
+                    command = lambda: sbbCallBack(args.title, args.sleep, args.winRaise))
     sbb.pack()
 
     Window.launch()

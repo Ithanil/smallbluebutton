0b68b84c98aac3512130b91fb246505a254c5556
Jan Kessler <jakessle@t-online.de>
2023-01-13 08:45:04 +0100

    make it an actual small blue button :)

--- smallbluebutton.py
+++ smallbluebutton.py
@@ -1,7 +1,20 @@
+#!/usr/bin/env python
+
+"""
+This script creates an overlay with a small blue button
+to toogle&check mute status in BigBlueButton conferences
+"""
+
 import tkinter as tk
 from overlay import Window
 
-win = Window()
-label = tk.Label(win.root, text="Window_0")
-label.pack()
+win = Window(size = (140, 140), alpha=0.85)
+
+# Load BBB Logo
+bbb_logo = tk.PhotoImage(file = 'logo.png')
+
+# Create button and image
+bbb_btn = tk.Button(win.root, image = bbb_logo, borderwidth = 0)
+bbb_btn.pack()
+
 Window.launch()
